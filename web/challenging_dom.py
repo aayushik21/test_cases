dd2583fe70fd2803100bbaa801aeb0cc5c4eac69
aayushik21 <bcs19ayushi.k@invertisuniversity.ac.in>
2023-05-23 16:46:39 +0530

    automation testing practice test cases.

---
+++ web/challenging_dom.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+try:
+    wait = WebDriverWait(driver, 10)
+    driver.get('https://the-internet.herokuapp.com')
+    dom = wait.until(EC.presence_of_element_located(
+        (By.LINK_TEXT, 'Challenging DOM')
+    ))
+    dom.click()
+
+    
+
+finally:
+    driver.quit()
